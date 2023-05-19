acd857b11d44764aca6f4d0b4625aba7f2e44db0
miguel <miguelpositivo02@gmail.com>
2023-05-18 21:08:02 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("digite os seus gastos mensais");
+decimal gastos = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("digite a porcentagem");
+double porcentagem = Convert.ToDouble(Console.ReadLine());
+decimal final=gastos  / Convert.ToDecimal(porcentagem/100);
+Console.Write($"voce precisa ter um valor investido de ${final}");
